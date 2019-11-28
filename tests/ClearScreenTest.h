f458cc1b60bff7a36280c84a695d73063b25d45c
coulsonwang <coulsonwang@126.com>
2019-11-28 13:39:22 +0800

    fix indentation

--- tests/ClearScreenTest.h
+++ tests/ClearScreenTest.h
@@ -8,21 +8,21 @@ NS_CC_BEGIN
 class ClearScreen: public TestBaseI
 {
  public:
-  DEFINE_CREATE_METHOD(ClearScreen)
-	ClearScreen();
-	~ClearScreen();
+    DEFINE_CREATE_METHOD(ClearScreen)
+    ClearScreen() = default;
+    ~ClearScreen();
 
  public:
-  virtual void tick(float dt) override;
-  virtual bool initialize(intptr_t windowHandle, GFXRect windowRect, int screenWidth, int screenHeight) override;
-  virtual void Destroy();
+     virtual void tick(float dt) override;
+     virtual bool initialize(intptr_t windowHandle, GFXRect windowRect, int screenWidth, int screenHeight) override;
+     virtual void Destroy();
 
  private:
-  GFXCommandBuffer* _commandBuffer = nullptr;
-  GFXFramebuffer* _fbo = nullptr;
-  GFXDevice* _device = nullptr;
+     GFXCommandBuffer* _commandBuffer = nullptr;
+     GFXFramebuffer* _fbo = nullptr;
+     GFXDevice* _device = nullptr;
 
-  float _time = 0.0f;
+     float _time = 0.0f;
 };
 
 NS_CC_END
